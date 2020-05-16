627d502911a1204106aac0e66a37f6b31d37694b
MarzDell <chodnickamarzena@yahoo.co.uk>
2020-05-16 14:55:27 +0000

    Doing few changes in custom.css and urls_reset

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -5,14 +5,14 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path(
-        'change-password/',
+        'change_password/',
         auth_views.PasswordChangeView.as_view(
             template_name='change-password.html',
             success_url = '/'
         ),
         name='change_password'
     ),
-    path('password-reset/',
+    path('password_reset/',
          auth_views.PasswordResetView.as_view(
              template_name='password_reset.html',
              subject_template_name='registration/password_reset_subject.txt',
@@ -20,17 +20,17 @@ urlpatterns = [
              # success_url='/login/'
          ),
          name='password_reset'),
-    path('password-reset/done/',
+    path('password_reset/done/',
          auth_views.PasswordResetDoneView.as_view(
              template_name='registration/password_reset_done.html'
          ),
          name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',
+    path('password_reset_confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
              template_name='registration/password_reset_confirm.html'
          ),
          name='password_reset_confirm'),
-    path('password-reset-complete/',
+    path('password_reset_complete/',
          auth_views.PasswordResetCompleteView.as_view(
              template_name='registration/password_reset_complete.html'
          ),
