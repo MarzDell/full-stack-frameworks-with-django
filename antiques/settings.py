154aae6d1ce66afac42d20f3af4c6fc2d26d4237
MarzDell <chodnickamarzena@yahoo.co.uk>
2020-05-23 17:23:15 +0000

    Added heroku name to Allowed_hosts

--- antiques/settings.py
+++ antiques/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
-import env
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -28,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("C9_HOSTNAME"), '127.0.0.1:8000']
+ALLOWED_HOSTS = [os.environ.get("C9_HOSTNAME"), '127.0.0.1:8000', marzdell-antiques.herokuapp.com]
 
 
 # Application definition
