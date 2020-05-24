b7e02e60804c8bbce11e0a6bac55f32e33b1cff2
MarzDell <chodnickamarzena@yahoo.co.uk>
2020-05-24 15:02:52 +0000

    removing media files

--- antiques/settings.py
+++ antiques/settings.py
@@ -145,10 +145,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIAFILES_LOCATION = 'media'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
